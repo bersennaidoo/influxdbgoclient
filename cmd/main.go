9ddd5274364dc47f7f57d2fbfeb181dd3cbb45de
Bersen Naidoo <bersen.naidoo@gmail.com>
2023-12-05 11:29:24 +0200

    Implemented WriteDataPoint method on influxdbstore and wrote data points to bucket

--- cmd/main.go
+++ cmd/main.go
@@ -3,14 +3,18 @@ package main
 import (
 	"os"
 
+	"github.com/bersennaidoo/influxdbgoclient/infrastructure/storage/influxstore"
 	"github.com/bersennaidoo/influxdbgoclient/physical/influxd"
 )
 
 func main() {
 	token := os.Getenv("INFLUXDB_TOKEN")
 	url := "http://localhost:8086"
+	org := "private"
+	bucket := "buc"
 
 	client := influxd.CreateInfluxDBClient(url, token)
 
 	inflst := influxstore.New(client)
+	inflst.WriteDataPoint(org, bucket)
 }
--- infrastructure/storage/influxstore/influxstore.go
+++ infrastructure/storage/influxstore/influxstore.go
@@ -1,16 +1,40 @@
 package influxstore
 
 import (
+	"context"
+	"log"
+	"time"
+
 	influxdb2 "github.com/influxdata/influxdb-client-go/v2"
-	_ "github.com/influxdata/influxdb-client-go/v2/api/write"
+	"github.com/influxdata/influxdb-client-go/v2/api/write"
 )
 
 type InfluxStore struct {
-	influxClient influxdb2.Client
+	client influxdb2.Client
 }
 
 func New(client influxdb2.Client) *InfluxStore {
 	return &InfluxStore{
-		influxClient: client,
+		client: client,
 	}
+}
+
+func (inf *InfluxStore) WriteDataPoint(org, bucket string) {
+	writeAPI := inf.client.WriteAPIBlocking(org, bucket)
+	for value := 0; value < 5; value++ {
+		tags := map[string]string{
+			"tagname1": "tagvalue1",
+		}
+		fields := map[string]interface{}{
+			"field1": value,
+		}
+		point := write.NewPoint("measurement1", tags, fields, time.Now())
+		time.Sleep(1 * time.Second) // separate points by 1 second
+
+		if err := writeAPI.WritePoint(context.Background(), point); err != nil {
+			log.Fatal(err)
+		}
+		log.Printf("Data Point stored in bucket")
+	}
+	log.Printf("Done")
 }
